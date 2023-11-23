beef002c80fa5bb5b8255cee51a56ef5179c323f
At0micSupra <43355748+At0micSupra@users.noreply.github.com>
2023-11-22 20:17:38 -0700

    added .cow verification function

--- cowfriends/cowfriends.cpp
+++ cowfriends/cowfriends.cpp
@@ -21,18 +21,33 @@ using namespace std; // i am harmful
 // cowpaths for homebrew, linux, macports
 const char* cowPaths[] = {"/usr/local/opt/cowsay/share/cows", "/usr/share/cowsay/cows", "/opt/local/share/cowsay/cows"};
 int cowPathsCount = 3;
+const std::string dotCow = ".cow";
+
+
+bool endsWithDotCow(std::string fName) {
+	if(fName.size() > dotCow.size()) {
+		return fName.compare(fName.size() - dotCow.size(), dotCow.size(), dotCow) == 0;
+	}
+}
+
 
 vector<string> getFriends(DIR* cowdir) {
 	vector<string> friends;
 	struct dirent *entry;
 	while((entry = readdir(cowdir)) != NULL) {
-		friends.push_back(entry->d_name);
+		auto fName = entry->d_name;
+		if(endsWithDotCow(fName)) {
+			friends.push_back(entry->d_name);
+		} else {
+			cout << "didn't end with .cow: " << fName << endl;
+		}
+		
 	}
 	closedir (cowdir);
 	return friends;
 }
 
-vectorstring> getCows() {
+vector<string> getCows() {
 	DIR *cowdir;
 	for(int i = 0; i < cowPathsCount; i++) {
 		if((cowdir = opendir(cowPaths[i])) != NULL) {
